a46d88c057ff9043e073a173832cc0a26ccc57f6
bhavanibodduru <146819738+bhavanibodduru@users.noreply.github.com>
2024-10-21 15:20:12 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Star {
+
+public static void main(String[] args) {
+    int i,j,row=4;
+
+
+for ( i = 0; i <=row; i++) { 
+for ( j = 0; j <= i; j++) { 
+
+System.out.print("*"); 
+
+}
+
+System.out.println(""); 
+
+}
+
+for ( i = 3; i <=3; i--) { 
+
+for ( j = 0; j <= i ; j++) { 
+
+System.out.print("*"); 
+
+}
+
+System.out.println("");
+
+}
+
+
+
+}
+
+}
