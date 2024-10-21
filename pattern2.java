077006915731839ba211dcf9fc05ed688221ddf5
bhavanibodduru <146819738+bhavanibodduru@users.noreply.github.com>
2024-10-21 14:46:38 +0530

    Create pattern2.java

---
+++ pattern2.java
@@ -0,0 +1,18 @@
+public class RightTrianglePattern   
+{   
+public static void main(String args[])   
+{   
+
+int i, j, row=5;   
+
+for(i=4; i<=4; i--)   
+{   
+  
+for(j=0; j<=i; j++)   
+{   
+System.out.print("* ");   
+}   
+System.out.println();   
+}   
+}   
+}  
